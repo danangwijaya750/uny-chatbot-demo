6732e359946a219a9761eb12d5b92a039a040975
Danang Wijaya <danangwijaya750@gmail.com>
2024-05-21 07:00:30 +0800

    add snackbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { AppBar, Toolbar, Typography, Container, TextField, Button, List, ListItem, ListItemText, ListItemAvatar, Avatar } from '@mui/material';
+import { AppBar, Toolbar, Typography, Container, TextField, Button, List, ListItem, ListItemText, ListItemAvatar, Avatar, Snackbar, IconButton } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 import { addMessage } from './actions';
 import apiClient from './apiClient';
 import messageObservable from './observer';
@@ -8,8 +9,11 @@ import BotIcon from '@mui/icons-material/Android';  // Import bot icon
 import TypingMessage from './TypingMessage';  // Import the new TypingMessage component
 import './App.css';
 
+const botAvatar = '/logo-uny.png'; 
 const App = () => {
   const [input, setInput] = useState('');
+  const [error, setError] = useState('');
+  const [open, setOpen] = useState(false);
   const dispatch = useDispatch();
   const messages = useSelector(state => state.messages.messages);
 
@@ -28,6 +32,8 @@ const App = () => {
   const sendMessage = async () => {
     const userMessage = input.trim();
     if (!userMessage) {
+      setError('Please input the message first!');
+      setOpen(true);
       return; // Do not send if the message is empty
     }
     
@@ -38,8 +44,17 @@ const App = () => {
       const response = await apiClient.postMessage(userMessage);
       messageObservable.notify({ text: response.data.response, sender: 'bot' });
     } catch (error) {
+      setError('Failed to send message. Please try again.');
+      setOpen(true);
       console.error('Error sending message:', error);
     }
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
   };
 
   return (
@@ -77,6 +92,23 @@ const App = () => {
         <Button onClick={sendMessage} variant="contained" color="primary">
           Send
         </Button>
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          open={open}
+          autoHideDuration={5000}
+          onClose={handleClose}
+          message={error}
+          action={
+            <>
+              <IconButton size="small" aria-label="close" color="inherit" onClick={handleClose}>
+                <CloseIcon fontSize="small" />
+              </IconButton>
+            </>
+          }
+        />
       </Container>
     </>
   );
